b6f13a14077f67167dd377818bd5b2a3a8b5e4eb
GabrilGiovanna <gabriel-ribeiro99@hotmail.com>
2021-05-27 23:33:26 +0100

    menu
    menu

--- main.c
+++ main.c
@@ -4,9 +4,19 @@
 
 main(){
 
-LIVRO livro;
+/*LIVRO livro;
 
-livro=NovoLivro();
+livro=NovoLivro(); */
+
+printf("-------Gestão de uma Livraria-------\n");
+
+printf("Pretende ir para que secção?\n");
+printf("1.Ficheiro\n");
+printf("2.Livros\n");
+printf("3.Clientes\n");
+printf("4.Encomendas\n");
+printf("5.Operações\n");
+
 
 
 
